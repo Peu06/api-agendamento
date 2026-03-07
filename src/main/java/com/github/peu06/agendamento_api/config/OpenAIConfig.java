75ed4d3d7a2e1ca91aedc70f9ff5577bda3bcb34
Peu06 <jpcaetano2006@gmail.com>
2026-03-07 00:03:08 -0300

    Add OpenAPI and CORS configuration; update application properties for Swagger

--- src/main/java/com/github/peu06/agendamento_api/config/OpenAIConfig.java
+++ src/main/java/com/github/peu06/agendamento_api/config/OpenAIConfig.java
@@ -1,4 +1,16 @@
 package com.github.peu06.agendamento_api.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://joni-unhoping-tachistoscopically.ngrok-free.dev"));
+    }
 }
