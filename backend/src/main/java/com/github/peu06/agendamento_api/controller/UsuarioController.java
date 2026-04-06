f61f9f992951adbbde4ff350e94cfbddf568f1b9
Peu06 <jpcaetano2006@gmail.com>
2026-04-06 16:00:33 -0300

    feat: Simplify imports in UsuarioController and ensure consistent formatting

--- backend/src/main/java/com/github/peu06/agendamento_api/controller/UsuarioController.java
+++ backend/src/main/java/com/github/peu06/agendamento_api/controller/UsuarioController.java
@@ -2,10 +2,7 @@ package com.github.peu06.agendamento_api.controller;
 
 import com.github.peu06.agendamento_api.model.Usuario;
 import com.github.peu06.agendamento_api.service.UsuarioService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,10 @@ public class UsuarioController {
     @GetMapping("/{id}")
     public Usuario getById(@PathVariable Long id){
         return usuarioService.getById(id);
+    }
+
+    @PostMapping
+    public Usuario post(@RequestBody Usuario usuario){
+        return usuarioService.salvar(usuario);
     }
 }
